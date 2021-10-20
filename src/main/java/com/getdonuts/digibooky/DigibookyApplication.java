9f8b82b4610ffbb837ce61cb36e0eff931265823
Andreeas Firoiu <andrfiro@gmail.com>
2021-10-20 11:20:44 +0200

    Initial commit

---
+++ src/main/java/com/getdonuts/digibooky/DigibookyApplication.java
@@ -0,0 +1,13 @@
+package com.getdonuts.digibooky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigibookyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigibookyApplication.class, args);
+    }
+
+}
