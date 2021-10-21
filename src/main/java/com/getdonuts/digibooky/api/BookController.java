2cf7bf1753ce582fd96d2a5e10221bb44b053363
Glenn Verhaeghe <glennverhaeghe@gmail.com>
2021-10-21 14:10:01 +0200

    Fixed test

--- src/main/java/com/getdonuts/digibooky/api/BookController.java
+++ src/main/java/com/getdonuts/digibooky/api/BookController.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.constraints.NotNull;
+//import javax.validation.constraints.NotNull;
 import java.util.Collection;
 
 @RestController
