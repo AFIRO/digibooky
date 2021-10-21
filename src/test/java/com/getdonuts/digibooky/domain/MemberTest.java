5f68b61c55c35a10b6ce8d07cfb50c32e6b43205
Glenn Verhaeghe <glennverhaeghe@gmail.com>
2021-10-21 10:15:00 +0200

    Updated MemberTest

--- src/test/java/com/getdonuts/digibooky/domain/MemberTest.java
+++ src/test/java/com/getdonuts/digibooky/domain/MemberTest.java
@@ -1,6 +1,6 @@
 package com.getdonuts.digibooky.domain;
 
-import org.apache.tomcat.jni.Address;
+import com.getdonuts.digibooky.services.EmailService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,8 +14,8 @@ class MemberTest {
     private String lastName = "TestLastName";
     private String email = "TestEMail@TestProvider.com";
     private String street = "TestStreet";
-    private int houseNr = 1;
-    private int postalCode = 1000;
+    private String houseNr = "1";
+    private String postalCode = "1000";
     private String city = "TestCity";
     private Address address;
 
@@ -34,11 +34,14 @@ class MemberTest {
         String email3 = "test@test.";
         String email4 = "@test.be";
         String email5 = "test.be";
+        String email6 = "test@@test.be";
+        String email7 = "te.st@te.st";
         EmailService es = new EmailService();
         //then
-        assertTrue(es.isValid(email1) && !isValid(email2) &&
-                !isValid(email3) && !isValid(email4) &&
-                !isValid(email5));
+        assertTrue(es.isValid(email1) && !es.isValid(email2) &&
+                !es.isValid(email3) && !es.isValid(email4) &&
+                !es.isValid(email5) && !es.isValid(email6) &&
+                !es.isValid(email7));
     }
 
     @Test
