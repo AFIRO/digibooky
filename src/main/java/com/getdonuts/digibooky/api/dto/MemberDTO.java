8732c267e5ba17663a5397ff91ced6a6843f58bd
Andreeas Firoiu <andrfiro@gmail.com>
2021-10-21 12:11:09 +0200

    Implemented member service, mapper, repo and parts of controller

---
+++ src/main/java/com/getdonuts/digibooky/api/dto/MemberDTO.java
@@ -0,0 +1,55 @@
+package com.getdonuts.digibooky.api.dto;
+
+public class MemberDTO {
+    private final String id;
+    private final String firstName;
+    private final String lastname;
+    private final String email;
+    private final String street;
+    private final String houseNumber;
+    private final String city;
+    private final String postcode;
+
+    public MemberDTO(String id, String firstName, String lastname, String email, String street, String houseNumber, String city, String postcode) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastname = lastname;
+        this.email = email;
+        this.street = street;
+        this.houseNumber = houseNumber;
+        this.city = city;
+        this.postcode = postcode;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+}
--- src/main/java/com/getdonuts/digibooky/domain/Member.java
+++ src/main/java/com/getdonuts/digibooky/domain/Member.java
@@ -1,7 +1,5 @@
 package com.getdonuts.digibooky.domain;
 
-import com.getdonuts.digibooky.services.MemberService;
-
 import java.util.UUID;
 
 public class Member {
@@ -16,24 +14,11 @@ public class Member {
 
     public Member(String INSS, String firstName, String lastName, String email, Address address) {
         this.id = UUID.randomUUID().toString();
-        if (MemberService.isINSSunique(INSS))
-            this.INSS = INSS;
+        this.INSS = INSS;
         this.firstName = firstName;
-        setLastName(lastName);
-        setEmail(email);
-        this.address = address;
-    }
-
-    public void setEmail(String email) {
-        if (MemberService.isEmailValid(email) && MemberService.isEmailUnique(email))
-            this.email = email;
-
-    }
-
-    public void setLastName(String lastName) {
-        if (lastName == null || lastName.isEmpty() || lastName.isBlank() )
-            throw new IllegalArgumentException("Last name must be filled in.");
         this.lastName = lastName;
+        this.email = email;
+        this.address = address;
     }
 
 
@@ -60,4 +45,6 @@ public class Member {
     public Address getAddress() {
         return address;
     }
+
+
 }
