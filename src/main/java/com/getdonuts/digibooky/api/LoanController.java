aad3476d2465e1569cdf2e7527dfc662a01f8f3c
Mert <mertdemirok80@gmail.com>
2021-10-22 13:11:37 +0200

    add Loan Controller

--- src/main/java/com/getdonuts/digibooky/api/LoanController.java
+++ src/main/java/com/getdonuts/digibooky/api/LoanController.java
@@ -1,4 +1,26 @@
 package com.getdonuts.digibooky.api;
 
+import com.getdonuts.digibooky.api.dto.BookDto;
+import com.getdonuts.digibooky.api.dto.CreateLoanDto;
+import com.getdonuts.digibooky.services.LoanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/loans")
 public class LoanController {
+
+    private final LoanService loanService;
+
+    @Autowired
+    public LoanController(LoanService loanService) {
+        this.loanService = loanService;
+    }
+
+    @PostMapping(produces = "application/json", consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public BookDto createBook(@RequestBody CreateLoanDto createLoanDto) {
+        return loanService.lendBook(createLoanDto);
+    }
 }
