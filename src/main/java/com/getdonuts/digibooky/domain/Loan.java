58c732bb27227688f48dba6054658f01dd765c11
Francois.Delepee <francoisdelepee@gmail.com>
2021-10-22 11:09:30 +0200

    Changes by freeh

--- src/main/java/com/getdonuts/digibooky/domain/Loan.java
+++ src/main/java/com/getdonuts/digibooky/domain/Loan.java
@@ -1,4 +1,11 @@
 package com.getdonuts.digibooky.domain;
 
+import java.time.LocalDate;
+
 public class Loan {
+//    private final String id;
+//    private final String userId;
+//    private final String bookId;
+//    private LocalDate lendDate;
+
 }
