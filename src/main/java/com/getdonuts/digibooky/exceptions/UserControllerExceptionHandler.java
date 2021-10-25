de0f0592b9154c0653e165696b8bc96b734af2d3
Francois.Delepee <francoisdelepee@gmail.com>
2021-10-25 10:47:11 +0200

    adding logging in the exception handlers

--- src/main/java/com/getdonuts/digibooky/exceptions/UserControllerExceptionHandler.java
+++ src/main/java/com/getdonuts/digibooky/exceptions/UserControllerExceptionHandler.java
@@ -11,11 +11,13 @@ public class UserControllerExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     protected void illegalArgumentException(IllegalArgumentException exception, HttpServletResponse response) throws Exception{
+        logger.error(exception.getMessage());
         response.sendError(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
     }
 
     @ExceptionHandler(AuthorisationException.class)
     protected void authorisationException(AuthorisationException exception, HttpServletResponse response) throws Exception{
+        logger.error(exception.getMessage());
         response.sendError(HttpStatus.FORBIDDEN.value(), exception.getMessage());
     }
 }
